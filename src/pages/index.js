f5d2e0fe29989bde50057944cc5158fcd43935db
王安南 <wangannan@nana.com>
2026-04-10 18:54:31 +0800

    Add trailing dash to blog link text in HomepageHeader component

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/blog">
-            Read My Blog ✍️
+            Read My Blog ✍️ - 
           </Link>
         </div>
       </div>
